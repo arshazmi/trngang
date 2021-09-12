394e6b2021775b04850e3d5e56136cb5bb1cb602
dawti <dawtiej@gmail.com>
2021-09-12 21:53:56 +0530

    student page, get student by teacher

---
+++ src/app/student/form/form.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute} from '@angular/router';
+import { Student } from 'src/app/models/student';
+import { StudentService } from 'src/app/service/student.service';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class SFormComponent implements OnInit {
+isAddMode: boolean=true;
+submitted = false;
+id: string="";
+
+  constructor(private studService: StudentService,private router: Router,private route: ActivatedRoute) { }
+
+  studentItem: Student = {id:'',firstName:'',lastName:'', dob:'',  email:'',  gender:'', mobile:'', address:'',  doj:''};
+  ngOnInit(): void {
+    console.log("reached form")
+      this.id = this.route.snapshot.params['id'];
+      // console.log(this.id)
+        this.isAddMode = !this.id;
+    // let idd = localStorage.getItem("editClassId");
+    // console.log(id);
+    this.studService.getStudent(this.id)
+    .subscribe((data)=>{
+      this.studentItem=JSON.parse(JSON.stringify(data));
+      console.log("item"+this.studentItem)
+  })
+  
+  }
+
+    onSubmit() {
+    this.submitted = true;
+
+    if (this.isAddMode) {
+        this.AddStudent();
+    }
+     else {
+        this.EditStudent(this.studentItem);
+    }
+}
+
+  AddStudent()
+  {    
+    this.studService.newStudent(this.studentItem);
+    console.log(this.studentItem,"newStudent");    
+    alert("Success");
+    this.router.navigate(['/student']);
+  }
+
+  EditStudent(item:any)
+  {
+    // let id = localStorage.getItem("editTeacherId"); 
+    let id=this.id;
+    // console.log("id"+id);
+    console.log("teacheritem"+item) ;
+    this.studService.editStudent(id,item);
+    alert("Success");
+    this.router.navigate(['student']);
+  }
+}
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StudentService } from '../service/student.service';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+  normal:boolean=true;
+  data:any;
+  toStudent :any ="Student";
+  teachername:string='hi';
+  student=[{
+    id:'',
+    firstName: '',
+    lastName: '',
+    dob: '',
+    email:'',
+    address: '',
+    gender:'',
+    mobile:'',
+    doj:''
+   }]
+
+  constructor(private router:Router,private route: ActivatedRoute,
+    private studService:StudentService) { }
+
+  ngOnInit(): void {
+    this.studService.getStudents().subscribe((data)=>{
+      console.log(data);
+      this.student=JSON.parse(JSON.stringify(data));
+  })
+
+  }
+
+  editStudent(item:any)
+  {console.log(item);
+    // localStorage.setItem("editTeacherId", item.id.toString());
+  //   this.route.params.subscribe(params => {
+  //     console.log("ID "+params['id'] ); 
+  // });
+  // let id=this.route.params.['id'];
+    this.router.navigate(['editstudent',item.id]);
+
+  }
+  deleteStudent(product:any)
+  {
+    this.studService.deleteStudent(product.id)
+      .subscribe((data) => {
+        console.log(data);
+        this.student = this.student.filter(p => p !== product);
+      })
+  
+
+  }
+
+  getStudOfTeacher(teachername:string){
+    console.log(teachername," obtained");
+    this.studService.getStudentByTeacher(teachername).subscribe(data=>{
+      console.log(data);
+      this.data=data;
+      this.normal=false;
+    })
+  }
+
+
+}
